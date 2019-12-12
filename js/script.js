3174cc29a55607a7f3f9f4e6c6086cc850dee32c
Andrew Galyan-Mann <andrew.galyanmann@icloud.com>
2019-12-12 00:13:15 -0500

    added in search element (not functional yet)

--- js/script.js
+++ js/script.js
@@ -5,6 +5,35 @@ FSJS project 2 - List Filter and Pagination
  //global variables to set framework for needed functions  
 const studentList = document.querySelectorAll('.student-item')
 const itemsPerPage = 10; //allows the developer to determine number of LI they want listed per page
+const searchAnchor = document.querySelector('h2').parentNode;
+
+//This function works to creat DOM elements more quickly while maintaining DRY and modular coding. 
+const createElement = elementType => {
+   const element = document.createElement(elementType);
+   return element;
+}
+
+const searchDiv = createElement('div');
+   searchDiv.className = 'student-search';
+const input = createElement('input');
+const button = createElement('button');
+
+input.placeholder = 'Search for students...';
+button.textContent = 'Search';
+
+searchAnchor.appendChild(searchDiv);
+searchDiv.appendChild(input);
+searchDiv.appendChild(button);
+
+const searchPage = (searchInput, names) => {
+   for (let i = 0; i < names.length; i++) {
+      names[i].classList.remove('match');
+      if (search.value.length !== 0 && names[i].textContent.toLowerCase() == searchInput.value.toLowerCase()) {
+         names[i].className = 'match';
+      }
+   }
+}
+
 
 //This function will limit the amount of LIs on a page based on global variable as well as recieve information to determine which page or set of LIs to display using index numbers. 
 const showPage = (list, page) => {
@@ -21,11 +50,7 @@ const showPage = (list, page) => {
 
 //This function will create dynamicly DOM elements to alter the look of the page based on the amount of students that are stored in the 'student-item' class. 
 const paginationLinks = list => {
-   //This function works internally to creat DOM elements more quickly while maintaining DRY and modular coding. 
-   const createElement = elementType => {
-      const element = document.createElement(elementType);
-      return element;
-   }
+
 
    //This section of code sets the number of pages needed and navigates and appends to a specific area in the DOM. (noted in the index.html file)
    const numberOfPages = Math.ceil((list.length / itemsPerPage));
